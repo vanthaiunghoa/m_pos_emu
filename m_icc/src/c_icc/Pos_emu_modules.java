e7e12ad636f1f031315dd0e3012fcb190569d513
balacsoft <balacsoft@gmail.com>
2018-07-30 21:49:15 +0200

    Use of a relative path for GSON lib. Remove test in ICC main function.

--- m_icc/src/c_icc/Pos_emu_modules.java
+++ m_icc/src/c_icc/Pos_emu_modules.java
@@ -44,48 +44,6 @@ public class Pos_emu_modules {
         C_err.Icc retIcc;
         String response;
 
-        /* TO REMOVE TODO FOR TEST */
-        CardTerminal m_terminal;
-        Card m_card;
-        CardChannel m_channel = null;
-
-        ResponseAPDU answer = null;
-        TerminalFactory terminalFactory = TerminalFactory.getDefault();
-	CardTerminals cardTerminals = terminalFactory.terminals();
-
-        m_terminal = cardTerminals.list().get(0);
-        String readerName = m_terminal.toString();
-        try {
-            m_card = m_terminal.connect("*");
-            m_channel = m_card.getBasicChannel();
-        } catch (CardException ex) {
-            Logger.getLogger(Pos_emu_modules.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        byte[] valueCB = {(byte)0xA0, 0x00, 0x00, 0x00, 0x42, 0x10, 0x10};
-        byte[] valueMC = {(byte)0xA0, 0x00, 0x00, 0x00, 0x04, 0x10, 0x10};
-        byte[] valueV  = {(byte)0xA0, 0x00, 0x00, 0x00, 0x03, 0x10, 0x10};
-           
-        
-        CommandAPDU cmd = new CommandAPDU(0x00, (byte)0xA4, 0x04, 0x00, valueV);
-        C_logger_stdout.LogInfo("TEST", "IccCmd=" + HexUtil.toHexString(cmd.getBytes()));
-        answer = m_channel.transmit(cmd);
-        C_logger_stdout.LogInfo("TEST", "IccRsp=" + HexUtil.toHexString(answer.getBytes()));
-
-        byte[] trame = {(byte)0x83, 0x00};
-        
-        cmd = new CommandAPDU(CLS_GPO,INS_GPO, 0, 0, trame, 0x0C);
-        
-        int cla = cmd.getCLA();
-        
-        C_logger_stdout.LogInfo("TEST", "IccCmd=" + HexUtil.toHexString(cmd.getBytes()));
-        answer = m_channel.transmit(cmd);
-        C_logger_stdout.LogInfo("TEST", "IccRsp=" + HexUtil.toHexString(answer.getBytes()));
-
-        return;
-        /* */
-        
-        /*
         // According to the parameter, use PC/SC or virtual Smart-Card
         if (smartCardType == C_icc.SmartCardManagementType.SMARTCARD_PCSC) {
             m_icc = new C_icc_pcsc("m_icc");
@@ -132,6 +90,5 @@ public class Pos_emu_modules {
         } else {
             C_logger_stdout.LogError(module_name, "Problem connecting to reader");
         }
-        */
     }    
 }
